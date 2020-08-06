e82b5351bdf5cd87fce4cff511f4c19c68bc10d3
Anastasiya <anastasiyauraleva@gmail.com>
2020-08-05 21:22:30 -0400

    used useEffect and axios to get data from wikipedia

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Accordion from './components/Accordion'
-import Search from '../components/Search'
+import Search from './components/Search'
 
 const items =[
     {
--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,7 +1,51 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
+import axios from 'axios';
 
 const Search =() =>{
     const[term, setTerm]=useState('');
+    const [results, setResults]=useState([]);
+
+    console.log(results)
+
+    //useEffect must be configured to tell the code when to execute it
+    //only has 2nd argument of an array - if array, it will run at initial render
+    //run at initial render, run after every rerender
+
+    useEffect(()=>{
+        //every time term changes, the code here will be run
+        //making a request
+        const search = async ()=>{
+            const {data} = await axios.get('https://en.wikipedia.org/w/api.php',{
+                params:{
+                    action: 'query',
+                    list: 'search',
+                    origin:'*',
+                    format:'json',
+                    srsearch: term,
+                }
+            });
+
+            setResults(data.query.search);
+        };
+        if (term){
+            search()
+        }
+    },[term]);
+
+    const renderedResults = results.map((result)=>{
+        return(
+            <div key={result.pageid} className='item'>
+                <div className='content'>
+                    <div className='header'>
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{ __html:result.snippet}}></span>
+                   
+                </div>
+        </div>
+        ) 
+    })
+
     return(
        <div>
             <div className='ui form'>
@@ -14,8 +58,13 @@ const Search =() =>{
                     />
                 </div>
             </div>
+            <div className='ui celled list'>
+            {renderedResults}
+            </div>
        </div>
     );
 };
+
+export default Search;
 
 //every time a new search is types, we would keep track of what is typed 
